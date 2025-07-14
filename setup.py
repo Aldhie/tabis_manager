c7ba250e1fd72641d4f52318582a1f7072883dd3
a7dh1e <aldhie@globaltelko.com>
2025-07-15 03:52:37 +0700

    feat: Reset repository to a clean and complete boilerplate

--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as f:
-    readme = f.read()
-
 setup(
-    name="tabis_manager",
-    version="0.0.1",
-    description="Custom application for Trabis.",
-    long_description=readme,
-    long_description_content_type="text/markdown",
-    author="Aldhie",
-    author_email="email@anda.com",
+    name='tabis_manager',
+    version='0.0.1',
+    description='Custom ERPNext App for Trabis',
+    author='Aldhie',
+    author_email='you@example.com',
     packages=find_packages(),
-    zip_safe=False,
     include_package_data=True,
-    install_requires=["frappe"],
+    zip_safe=False,
+    install_requires=[],
 )
+
--- tabis_manager/hooks.py
+++ tabis_manager/hooks.py
@@ -1,12 +1,10 @@
-from . import __version__ as app_version
-
 app_name = "tabis_manager"
 app_title = "Tabis Manager"
 app_publisher = "Aldhie"
-app_description = "Custom application for Trabis."
-app_email = "email@anda.com"
+app_description = "Custom App for ERP"
+app_email = "you@example.com"
 app_license = "MIT"
 
-# Biarkan kosong untuk sekarang
-app_include_css = []
-app_include_js = []
+# Uncomment if using frontend
+# app_include_js = "/assets/tabis_manager/js/tabis_manager.js"
+
