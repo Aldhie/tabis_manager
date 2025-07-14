f12f5d065e5c633ff1ceeec894033e6998f5294e
a7dh1e <aldhie@globaltelko.com>
2025-07-14 19:06:28 +0700

    feat: Add required hooks.py file

---
+++ tabis_manager/hooks.py
@@ -0,0 +1,11 @@
+# hooks.py
+app_name = "tabis_manager"
+app_title = "Tabis Manager"
+app_publisher = "Tabis"
+app_description = "Custom application for Trabis."
+app_email = "apps-support@globaltelko.com"
+app_license = "MIT"
+
+# Biarkan kosong jika Anda belum memiliki file JS/CSS kustom
+app_include_css = []
+app_include_js = []
